faa422ab50764752309ed17b5f1c84e0f91da9c7
Moses <moses.skoda@gmail.com>
2025-10-14 22:48:50 +0100

    add shadcn + auto dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,30 @@
+import { useEffect } from "react";
+import { Routes, Route, BrowserRouter } from "react-router";
+
 export default function App() {
-	return <div>hello world</div>;
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+		const handleChange = (e: MediaQueryListEvent) => {
+			if (e.matches) {
+				document.documentElement.classList.add("dark");
+			} else {
+				document.documentElement.classList.remove("dark");
+			}
+		};
+
+		mediaQuery.addEventListener("change", handleChange);
+
+		return () => {
+			mediaQuery.removeEventListener("change", handleChange);
+		};
+	}, []);
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<div>hello world</div>} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+
+if (typeof window !== "undefined" && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+	document.documentElement.classList.add("dark");
+}
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<App />} />
-			</Routes>
-		</BrowserRouter>
+		<App />
 	</StrictMode>,
 );
